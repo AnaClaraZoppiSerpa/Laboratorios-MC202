a3ee1fff14dc61c9bd79af2822cbce6962b33e8b
Gabriel <gabrieloliveiradossantosrv@gmail.com>
2017-11-03 19:40:56 -0200

    IDE files

---
+++ Lab07/cmake-build-debug/CMakeFiles/Lab07.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab07.dir/main.c.obj"
+  "Lab07.pdb"
+  "Lab07.exe"
+  "Lab07.exe.manifest"
+  "libLab07.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab07.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
