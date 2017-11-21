1e40a2a46bac89a51082b88c277a6700ec28349a
Ana Clara Zoppi Serpa <anaclara.zoppiserpa@gmail.com>
2017-11-21 21:52:59 -0200

    Teste porque não sei o que tá havendo com o CMake ele trocou uns negócio

---
+++ Lab08/cmake-build-debug/CMakeFiles/Lab08.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab08.dir/main.c.obj"
+  "Lab08.pdb"
+  "Lab08.exe"
+  "Lab08.exe.manifest"
+  "libLab08.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab08.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
